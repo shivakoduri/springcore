7a933d82ae2c03620d3e7471dbf43e967b92ab0c
shiva <koduri.shiva@gmail.com>
2018-01-28 22:50:32 +0530

    Initial Commit

---
+++ src/main/java/com/myprojects/spring/springcore/SpringcoreApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.spring.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreApplication.class, args);
+	}
+}

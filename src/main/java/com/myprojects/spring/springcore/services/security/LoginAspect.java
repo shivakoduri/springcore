f435fe9b94bcbdf936ae2f7b016457cd979a6139
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-25 20:47:28 +0530

    Spring AOP Example

---
+++ src/main/java/com/myprojects/spring/springcore/services/security/LoginAspect.java
@@ -0,0 +1,33 @@
+package com.myprojects.spring.springcore.services.security;
+
+import org.aspectj.lang.annotation.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoginAspect {
+
+    @Pointcut("execution(* org.springframework.security.authentication.AuthenticationProvider.authenticate(..))")
+    public void doAuthenticate(){
+
+    }
+
+    @Before("com.myprojects.spring.springcore.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logBefore(Authentication authentication){
+
+        System.out.println("This is before the Authenticate Method: authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterReturning(value = "com.myprojects.spring.springcore.services.security.LoginAspect.doAuthenticate()",
+            returning = "authentication")
+    public void logAfterAuthenticate( Authentication authentication){
+        System.out.println("This is after the Authenticate Method authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterThrowing("com.myprojects.spring.springcore.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logAuthenicationException(Authentication authentication){
+        String userDetails = (String) authentication.getPrincipal();
+        System.out.println("Login failed for user: " + userDetails);
+    }
+}

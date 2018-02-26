62e15b8f9aaa7fd2a61c4bbade81404383856a92
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-26 22:13:21 +0530

    Spring Task Scheduling

---
+++ src/main/java/com/myprojects/spring/springcore/services/security/LoginService.java
@@ -0,0 +1,6 @@
+package com.myprojects.spring.springcore.services.security;
+
+public interface LoginService {
+
+    void resetFailedLogins();
+}
---
+++ src/main/java/com/myprojects/spring/springcore/services/security/LoginServiceImpl.java
@@ -0,0 +1,38 @@
+package com.myprojects.spring.springcore.services.security;
+
+import com.myprojects.spring.springcore.domain.User;
+import com.myprojects.spring.springcore.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Scheduled(fixedRate = 60000)
+    @Override
+    public void resetFailedLogins() {
+        System.out.println("Checking for locked accounts");
+
+        List<User> userList = (List<User>) userService.listAll();
+
+        userList.forEach(user -> {
+            if(!user.getEnabled() && user.getFailedLoginAttempts() > 0){
+                System.out.println("Resetting failed attempts for user: " + user.getUsername());
+                user.setFailedLoginAttempts(0);
+                user.setEnabled(true);
+                userService.saveOrUpdate(user);
+            }
+        });
+
+    }
+}

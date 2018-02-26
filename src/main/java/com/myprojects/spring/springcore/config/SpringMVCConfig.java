de93403aab9590bfff678883c7b8695f1dd1d936
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-26 23:43:32 +0530

    Removing Spring Boot Spring MVC configuration

---
+++ src/main/java/com/myprojects/spring/springcore/config/SpringMVCConfig.java
@@ -0,0 +1,9 @@
+package com.myprojects.spring.springcore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class SpringMVCConfig {
+}
